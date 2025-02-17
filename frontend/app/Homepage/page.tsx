2f33e2cd549870aed0af9ade110db2a5bdc24365
pulindu-seniya-silva <silva.m.k.p.s@gmail.com>
2025-02-17 12:26:00 +0530

    error fix

--- frontend/app/Homepage/page.tsx
+++ frontend/app/Homepage/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="coming-soon">
       <h1>Uni Care Homepage Coming Soon!</h1>
-      <p>We're working on something exciting. Stay tuned for updates!</p>
+      <p>We&apos;re working on something exciting. Stay tuned for updates!</p>
       <p>📧 Contact : pulinduseniya@gmail.com.com</p>
     </div>
   );
