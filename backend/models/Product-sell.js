eb87ab44718440cd314fafbb77dfa8911516e753
pulindu-seniya-silva <silva.m.k.p.s@gmail.com>
2025-02-25 17:28:50 +0530

    frat(Fix) mongoose and framer-motion dependency errors

--- backend/models/Product-sell.js
+++ backend/models/Product-sell.js
@@ -2,51 +2,57 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const productSchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    description: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    price: {
-        type: Number,
-        required: true,
-        min: 0
-    },
-    category: {
-        type: String,
-        required: true,
-        enum: ['Guitar', 'Piano', 'Drums', 'Violin', 'Accessories'] // Example categories
-    },
-    stock: {
-        type: Number,
-        required: true,
-        min: 0,
-        default: 1
-    },
-    images: [{
-        type: String, // URLs to product images
-        required: true
-    }],
-    ratings: {
-        type: Number,
-        min: 0,
-        max: 5,
-        default: 0
-    },
-    reviews: [{
-        user: { type: Schema.Types.ObjectId, ref: 'User' },
-        comment: { type: String, required: true },
-        rating: { type: Number, min: 0, max: 5, required: true }
-    }],
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  category: {
+    type: String,
+    required: true,
+    enum: [
+      'Herbal Supplements',
+      'Ayurvedic Oils',
+      'Personal Care Products',
+      'Ayurvedic Teas and Beverages',
+      'Ayurvedic Medicines',
+    ], // Updated categories
+  },
+  stock: {
+    type: Number,
+    required: true,
+    min: 0,
+    default: 1,
+  },
+  images: [{
+    type: String, // URLs to product images
+    required: true,
+  }],
+  ratings: {
+    type: Number,
+    min: 0,
+    max: 5,
+    default: 0,
+  },
+  reviews: [{
+    user: { type: Schema.Types.ObjectId, ref: 'User' },
+    comment: { type: String, required: true },
+    rating: { type: Number, min: 0, max: 5, required: true },
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 const Product = mongoose.model('Product', productSchema);
