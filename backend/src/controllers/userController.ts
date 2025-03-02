974816718b21753eac75ecea17ac4992b0245b53
Tombstone119 <kodagodayohan@gmail.com>
2025-03-03 01:23:15 +0530

    fix(services): fix file route error

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -2,7 +2,7 @@ import HttpStatusCodes from "../util/statusCodes.ts";
 
 import userService from "../services/userService.ts";
 import { Request, Response } from "express";
-import { handleError } from "../util/errorHandler.ts";
+import { handleError } from "../util/errorHandler.ts"
 
 export const getAllUsers = async (_: Request, res: Response) => {
   try {
