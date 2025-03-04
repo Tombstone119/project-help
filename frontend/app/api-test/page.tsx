e9aaaffac38e973c29cf20c3a1a9e78b291c435c
Tombstone119 <kodagodayohan@gmail.com>
2025-03-04 18:31:53 +0530

    fix: fix environment variable issue

--- frontend/app/api-test/page.tsx
+++ frontend/app/api-test/page.tsx
@@ -3,7 +3,7 @@ import TestForm from "@/components/ui/forms/testForm";
 export default async function page() {
   //const apiresponse = await fetch(`${process.env.API_URL}/products`);
 
-  const { products } = await fetch(`${process.env.API_URL}/products`).then(
+  const { products } = await fetch('http://localhost:8081/api/products').then(
     (res) => res.json()
   );
 
