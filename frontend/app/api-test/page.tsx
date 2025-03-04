7da65f55289d1aa87a79b3f6ead7d65114876a82
Tombstone119 <kodagodayohan@gmail.com>
2025-03-04 20:19:25 +0530

    fix: try fixing build issue by passing a const instead of e variable

--- frontend/app/api-test/page.tsx
+++ frontend/app/api-test/page.tsx
@@ -1,9 +1,10 @@
 import TestForm from "@/components/ui/forms/testForm";
 
 export default async function page() {
+  const apiUrl = process.env.API_URL || "http://localhost:8081/api";
   //const apiresponse = await fetch(`${process.env.API_URL}/products`);
 
-  const { products } = await fetch('http://localhost:8081/api/products').then(
+  const { products } = await fetch(`${apiUrl}/products`).then(
     (res) => res.json()
   );
 
