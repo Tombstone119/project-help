526109b9fb7856a94ae7c6a3ff44e2f70ed186a1
Tombstone119 <kodagodayohan@gmail.com>
2025-03-05 16:07:25 +0530

    fix: fix accidently deleting files

--- backend/src/services/UserService.ts
+++ backend/src/services/UserService.ts
@@ -1,73 +1,46 @@
-import { RouteError } from '@src/common/route-errors';
-import HttpStatusCodes from '@src/common/HttpStatusCodes';
+import { IUser } from "../types/user.ts";
+import User from "../models/userModel.ts";
 
-import UserRepo from '@src/repos/UserRepo';
-import { IUser } from '@src/models/User';
-
-
-/******************************************************************************
-                                Variables
-******************************************************************************/
-
-export const USER_NOT_FOUND_ERR = 'User not found';
-
-
-/******************************************************************************
-                                Functions
-******************************************************************************/
-
-/**
- * Get all users.
- */
-function getAll(): Promise<IUser[]> {
-  return UserRepo.getAll();
+async function getAll(): Promise<any> {
+  const allUsers = await User.find();
+  return allUsers;
 }
 
-/**
- * Add one user.
- */
-function addOne(user: IUser): Promise<void> {
-  return UserRepo.add(user);
+async function findById(id: string): Promise<any> {
+  const user = await User.findById(id);
+  return user;
 }
 
-/**
- * Update one user.
- */
-async function updateOne(user: IUser): Promise<void> {
-  const persists = await UserRepo.persists(user.id);
-  if (!persists) {
-    throw new RouteError(
-      HttpStatusCodes.NOT_FOUND,
-      USER_NOT_FOUND_ERR,
-    );
-  }
-  // Return user
-  return UserRepo.update(user);
+async function create(user: IUser): Promise<any> {
+  const newUser = new User({
+    name: user.name,
+    email: user.email,
+  });
+  await newUser.save();
+  return newUser;
 }
 
-/**
- * Delete a user by their id.
- */
-async function _delete(id: number): Promise<void> {
-  const persists = await UserRepo.persists(id);
-  if (!persists) {
-    throw new RouteError(
-      HttpStatusCodes.NOT_FOUND,
-      USER_NOT_FOUND_ERR,
-    );
-  }
-  // Delete user
-  return UserRepo.delete(id);
+async function update({ id, user }: { id: string; user: IUser }): Promise<any> {
+  const updatedUser = await User.findByIdAndUpdate(
+    id,
+    {
+      name: user.name,
+      email: user.email,
+    },
+    { new: true }
+  );
+  return updatedUser;
 }
 
-
-/******************************************************************************
-                                Export default
-******************************************************************************/
+async function findAndDelete(id: string): Promise<any> {
+  const deletedUser = await User.findByIdAndDelete(id);
+  return deletedUser;
+}
 
 export default {
   getAll,
-  addOne,
-  updateOne,
-  delete: _delete,
-} as const;
+  findById,
+  create,
+  update,
+  findAndDelete,
+};
